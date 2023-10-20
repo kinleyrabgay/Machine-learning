87cfb9141c5afe2037427084fe48756bcccfd0f7
kinleyrabgay <fsd.rabgay@gmail.com>
2023-10-20 12:18:59 +0600

    visualization

--- logistic/cognitive_impairement/visualize.r
+++ logistic/cognitive_impairement/visualize.r
@@ -7,11 +7,55 @@ library(readr)
 # Visualization function
 visualize_data <- function(data) {
 
-  # Summary statistic
+  # Specify the directory where you want to save the file
+  output_directory <- "visualization"
+
+  # Check if the directory exists, and if not, create it
+  if (!file.exists(output_directory)) {
+    dir.create(output_directory)
+  }
+
+  # Summary
   summary(data)
-  ggplot(data, aes(Age, fill = MMSE_class_2)) +
+
+  ########################################### Age Vs MMSE_class_2 ###########################################
+
+  cat("Generating AGE vs MMSE insights...... \n")
+  p <- ggplot(data, aes(Age, fill = MMSE_class_2)) +
     geom_bar() +
     coord_flip()
+  age_vs_MMSE_class_2_plot <- file.path(output_directory, "age_vs_MMSE_class_2_plot.pdf")
+  ggsave(age_vs_MMSE_class_2_plot, plot = p)
+  cat("Completed generating the insight at => visualization/age_vs_MMSE_class_2_plot \n")
+
+  ###########################################################################################################
+
+
+  ########################################### Age Count Insights ############################################
+
+  cat("Generating Age Count Stats insights inside data....\n")
+
+  # Check if the age_counts.txt file exists
+  if (file.exists("age_counts.txt")) {
+    age_counts <- read.table("age_counts.txt", header = TRUE)
+    p <- ggplot(age_counts, aes(x = Age, y = count)) +
+      geom_bar(stat = "identity", fill = "skyblue") +
+      labs(title = "Age Counts", x = "Age", y = "Count") +
+      theme_minimal() +
+      geom_text(aes(label = count), vjust = -0.5, size = 2)
+
+    # Specify the output file path and save the plot
+    age_count_plot <- file.path(output_directory, "age_counts_plot.pdf")
+    ggsave(age_count_plot, plot = p)
+    cat("Completed generating the insight at => visualization/age_counts_plot\n")
+  } else {
+    cat("The age_counts.txt file does not exist. Please run pre-processing.r file.\n")
+  }
+
+  ############################################### End #######################################################
+
+
+
 
   # Your data visualization code here
   # You can create plots using ggplot2 or other libraries
@@ -56,9 +100,15 @@ visualize_data <- function(data) {
 
 }
 
-############################ Main script #############################
+################################################# Main script #################################################
 
-data <- read.csv("preprocessed_data/preprocessed_data.csv")
+if (file.exists("preprocessed_data/preprocessed_data.csv")) {
+  data <- read.csv("preprocessed_data/preprocessed_data.csv")
+  
+  # Visualization
+  visualize_data(data)
+} else {
+  cat("preprocessed_data.csv don't exit, please run pre-processing.r file \n")
+}
 
-# Visualization
-visualize_data(data)
+################################################################################################################
